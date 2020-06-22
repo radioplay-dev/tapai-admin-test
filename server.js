ce2eb4ed8e65c6ce17a902633b2a7231d042f425
magnusahlstrom <magnus@atuin.se>
2020-06-22 13:12:28 +0200

    gif

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello U4!');
+  res.send('Hello U4!<br/><img src="/_next/giphy.gif">');
 });
 
 app.listen(PORT, HOST);
